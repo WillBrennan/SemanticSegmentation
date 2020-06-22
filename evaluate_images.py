8dd375cdae8f8660a2fc24e89f29cd725c24e083
WillBrennan <WillBrennan@users.noreply.github.com>
2020-06-22 01:08:01 +0100

    evaluate-images - crop images to be a multiple of 32

--- evaluate_images.py
+++ evaluate_images.py
@@ -32,6 +32,19 @@ def find_files(dir_path: pathlib.Path, file_exts):
         yield from dir_path.rglob(f'*{file_ext}')
 
 
+def _load_image(image_path: pathlib.Path):
+    image = cv2.imread(str(image_path))
+    assert image is not None
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    image_width = (image.shape[1] // 32) * 32
+    image_height = (image.shape[0] // 32) * 32
+
+    image = image[:image_height, :image_width]
+    return image
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     args = parse_args()
@@ -46,8 +59,7 @@ if __name__ == '__main__':
 
     fn_image_transform = transforms.Compose(
         [
-            transforms.Lambda(lambda image_path: cv2.imread(str(image_path))),
-            transforms.Lambda(lambda image: cv2.cvtColor(image, cv2.COLOR_BGR2RGB)),
+            transforms.Lambda(lambda image_path: _load_image(image_path)),
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ]
